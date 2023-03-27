82724f8934e27f3cbb74b5515f8b31ea59ac10e4
NSW578 <nelio.sager.20@gmail.com>
2023-03-28 00:16:44 +0200

    Failed test on ConveyorBelts

---
+++ Assets/Scripts/Nelio/ConveyorBelt.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConveyorBelt : MonoBehaviour
+{
+
+    public float speed;
+    public Vector3 direction;
+    public List<GameObject> onBelt;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for(int i = 0; i <= onBelt.Count -1; i++)
+        {
+            onBelt[i].GetComponent<Rigidbody>().velocity = speed * direction * Time.deltaTime;
+        }
+    }
+
+    // När något kolliderar med beltet
+    private void OnCollisionEnter(Collision collision)
+    {
+        onBelt.Add(collision.gameObject);
+    }
+
+    // När något lämnar beltet
+    private void OnCollisionExit(Collision collision)
+    {
+        onBelt.Remove(collision.gameObject);
+    }
+}
