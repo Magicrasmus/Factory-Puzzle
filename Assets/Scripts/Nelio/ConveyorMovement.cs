da829b1970eb9457a3b8be62ce2394a3c7380903
NSW578 <nelio.sager.20@gmail.com>
2023-04-08 21:22:20 +0200

    Test för movemnt men conveyor belts(Fail)

---
+++ Assets/Scripts/Nelio/ConveyorMovement.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConveyorMovement : MonoBehaviour
+{
+
+    [SerializeField]
+    float moveSpeed = 0.25f;
+
+    Vector3 targetPosition;
+    Vector3 startPosition;
+    bool moving;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (moving)
+        {
+            if (Vector3.Distance(startPosition, transform.position) > 1f)
+            {
+                transform.position = targetPosition;
+                moving = false;
+                return;
+            }
+
+            transform.position += (targetPosition - startPosition) * moveSpeed * Time.deltaTime;
+            return;
+
+        }
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "player")
+        {
+            targetPosition = transform.position;
+            startPosition = transform.position;
+            moving = true;
+        }
+    }
+}
