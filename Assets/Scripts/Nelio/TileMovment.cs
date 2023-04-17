dcce627270309aec7f59d24d990900917dc9d90f
NSW578 <nelio.sager.20@gmail.com>
2023-04-18 00:21:52 +0200

    Testat med lite collisions

--- Assets/Scripts/Nelio/TileMovment.cs
+++ Assets/Scripts/Nelio/TileMovment.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class TileMovment : MonoBehaviour
 {
     [SerializeField]
-    float moveSpeed = 0.25f;
+    float moveSpeed = 2f;
     [SerializeField]
     float rayLenght = 1.4f;
 
@@ -23,6 +23,18 @@ public class TileMovment : MonoBehaviour
     void Update()
     {
 
+        Vector3 direction = Vector3.forward;
+        Ray theRay = new Ray(transform.position, transform.TransformDirection(direction * rayLenght));
+
+        //if (Physics.Raycast(theRay, out RaycastHit hit, rayLenght))
+        //{
+        //    if (hit.collider.tag == "movable")
+        //    {
+        //        moving = true;
+        //    }
+        //}
+
+
         if (moving)
         {
             if (Vector3.Distance(startPosition, transform.position) > 1f)
@@ -39,42 +51,48 @@ public class TileMovment : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.W))
         {
-            if (!Physics.Raycast(transform.position, Vector3.forward, rayLenght))
+            //if (!Physics.Raycast(transform.position, Vector3.forward, rayLenght) /*&& transform.tag == "movable"*/)
+            //if (Physics.Raycast(theRay, out RaycastHit hit, rayLenght))
             {
-                targetPosition = transform.position + Vector3.forward;
-                startPosition = transform.position;
-                moving = true;
+                //if (hit.collider.tag == "movable")
+                //{
+                    targetPosition = transform.position + Vector3.forward;
+                    startPosition = transform.position;
+                    moving = true;
+
+                //}
+
             }
         }
 
         else if (Input.GetKeyDown(KeyCode.S))
         {
-            if (!Physics.Raycast(transform.position, Vector3.back, rayLenght))
-            {
-                targetPosition = transform.position + Vector3.back;
-                startPosition = transform.position;
-                moving = true;
-            }
+            //if (!Physics.Raycast(transform.position, Vector3.back, rayLenght))
+            //{
+            targetPosition = transform.position + Vector3.back;
+            startPosition = transform.position;
+            moving = true;
+            //}
         }
 
         else if (Input.GetKeyDown(KeyCode.A))
         {
-            if (!Physics.Raycast(transform.position, Vector3.left, rayLenght))
-            {
-                targetPosition = transform.position + Vector3.left;
-                startPosition = transform.position;
-                moving = true;
-            }
+            //if (!Physics.Raycast(transform.position, Vector3.left, rayLenght))
+            //{
+            targetPosition = transform.position + Vector3.left;
+            startPosition = transform.position;
+            moving = true;
+            //}
         }
 
         else if (Input.GetKeyDown(KeyCode.D))
         {
-            if (!Physics.Raycast(transform.position, Vector3.right, rayLenght))
-            {
-                targetPosition = transform.position + Vector3.right;
-                startPosition = transform.position;
-                moving = true;
-            }
+            //if (!Physics.Raycast(transform.position, Vector3.right, rayLenght))
+            //{
+            targetPosition = transform.position + Vector3.right;
+            startPosition = transform.position;
+            moving = true;
+            //}
         }
 
     }
