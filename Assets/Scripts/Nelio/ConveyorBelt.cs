dcf9296eac34af1ca3a9b9fc54e9f7f40aa0a6a6
Bob3311 <johan.p030619@gmail.com>
2023-04-18 12:18:41 +0200

    Added ability to enable and disable conveyorbelts with "E" key

--- Assets/Scripts/Nelio/ConveyorBelt.cs
+++ Assets/Scripts/Nelio/ConveyorBelt.cs
@@ -8,22 +8,32 @@ public class ConveyorBelt : MonoBehaviour
     public GameObject conveyorbelt;
     public Transform endpoint;
     public float speed;
+    public bool active;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        active = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (active) { active = false; }
+            else if (!active) { active = true; }
+        }
 
+    }
+        
     void OnTriggerStay(Collider other)
     {
-        other.transform.position = Vector3.MoveTowards(other.transform.position, endpoint.position, speed * Time.deltaTime);
+        if (active)
+        {
+            other.transform.position = Vector3.MoveTowards(other.transform.position, endpoint.position, speed * Time.deltaTime);
+        }
     }
+        
 }
--- Assets/Scripts/Nelio/TileMovment.cs
+++ Assets/Scripts/Nelio/TileMovment.cs
@@ -12,87 +12,96 @@ public class TileMovment : MonoBehaviour
     Vector3 targetPosition;
     Vector3 startPosition;
     bool moving;
+    public bool active;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        active = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        Vector3 direction = Vector3.forward;
-        Ray theRay = new Ray(transform.position, transform.TransformDirection(direction * rayLenght));
-
-        //if (Physics.Raycast(theRay, out RaycastHit hit, rayLenght))
-        //{
-        //    if (hit.collider.tag == "movable")
-        //    {
-        //        moving = true;
-        //    }
-        //}
-
-
-        if (moving)
+        if (Input.GetKeyDown(KeyCode.E))
         {
-            if (Vector3.Distance(startPosition, transform.position) > 1f)
-            {
-                transform.position = targetPosition;
-                moving = false;
-                return;
-            }
-
-            transform.position += (targetPosition - startPosition) * moveSpeed * Time.deltaTime;
-            return;
-
+            if (active) { active = false; }
+            else if (!active) { active = true; }
         }
 
-        if (Input.GetKeyDown(KeyCode.W))
+        Vector3 direction = transform.forward;
+        Ray theRay = new Ray(transform.position, transform.TransformDirection(direction * rayLenght));
+
+        if (Physics.Raycast(theRay, out RaycastHit hit, rayLenght))
         {
-            //if (!Physics.Raycast(transform.position, Vector3.forward, rayLenght) /*&& transform.tag == "movable"*/)
-            //if (Physics.Raycast(theRay, out RaycastHit hit, rayLenght))
+            if (hit.collider.tag == "movable")
             {
-                //if (hit.collider.tag == "movable")
-                //{
+                moving = true;
+            }
+        }
+
+        if (!active)
+        {
+            if (moving)
+            {
+                if (Vector3.Distance(startPosition, transform.position) > 1f)
+                {
+                    transform.position = targetPosition;
+                    moving = false;
+                    return;
+                }
+
+                transform.position += (targetPosition - startPosition) * moveSpeed * Time.deltaTime;
+                return;
+
+            }
+
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                //if (!Physics.Raycast(transform.position, Vector3.forward, rayLenght) /*&& transform.tag == "movable"*/)
+                //if (Physics.Raycast(theRay, out RaycastHit hit, rayLenght))
+                {
+                    //if (hit.collider.tag == "movable")
+                    //{
+
                     targetPosition = transform.position + Vector3.forward;
                     startPosition = transform.position;
                     moving = true;
 
-                //}
+                    //}
 
+                }
             }
-        }
 
-        else if (Input.GetKeyDown(KeyCode.S))
-        {
-            //if (!Physics.Raycast(transform.position, Vector3.back, rayLenght))
-            //{
-            targetPosition = transform.position + Vector3.back;
-            startPosition = transform.position;
-            moving = true;
-            //}
-        }
+            else if (Input.GetKeyDown(KeyCode.S))
+            {
+                //if (!Physics.Raycast(transform.position, Vector3.back, rayLenght))
+                //{
+                targetPosition = transform.position + Vector3.back;
+                startPosition = transform.position;
+                moving = true;
+                //}
+            }
 
-        else if (Input.GetKeyDown(KeyCode.A))
-        {
-            //if (!Physics.Raycast(transform.position, Vector3.left, rayLenght))
-            //{
-            targetPosition = transform.position + Vector3.left;
-            startPosition = transform.position;
-            moving = true;
-            //}
-        }
+            else if (Input.GetKeyDown(KeyCode.A))
+            {
+                //if (!Physics.Raycast(transform.position, Vector3.left, rayLenght))
+                //{
+                targetPosition = transform.position + Vector3.left;
+                startPosition = transform.position;
+                moving = true;
+                //}
+            }
 
-        else if (Input.GetKeyDown(KeyCode.D))
-        {
-            //if (!Physics.Raycast(transform.position, Vector3.right, rayLenght))
-            //{
-            targetPosition = transform.position + Vector3.right;
-            startPosition = transform.position;
-            moving = true;
-            //}
+            else if (Input.GetKeyDown(KeyCode.D))
+            {
+                //if (!Physics.Raycast(transform.position, Vector3.right, rayLenght))
+                //{
+                targetPosition = transform.position + Vector3.right;
+                startPosition = transform.position;
+                moving = true;
+                //}
+            }
         }
 
     }
